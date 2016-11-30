72b04bdaf2b8ae2a5206a1ee46241d018e9fef3e
YinTokey <YinjianChen02@gmail.com>
2016-11-29 17:26:19 -0800

    fixes

--- DropMenu-demo/ViewController.m
+++ DropMenu-demo/ViewController.m
@@ -20,11 +20,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.backgroundImgView.contentMode = UIViewContentModeScaleAspectFill;
+    self.backgroundImgView.clipsToBounds = YES;
+    
+    
+    
     NSArray *arr = [NSArray arrayWithObjects:@"CELTICS",@"CLIPPERS",@"WARRIORS",nil];
     self.dropMenu = [[YTDropMenu alloc ]initWithDataSource:arr NavTitile:@"Teams" ToNavigationController:self.navigationController];
     NSArray *array = @[@"1",@"2",@"3"];
     [self.dropMenu setCelliconsArray:array];
+    [self.dropMenu setAllColor:[UIColor colorWithRed:55/255.0f green:112/255.0f blue:126/255.0f alpha:1.0f]];
     self.dropMenu.delegate = self;
 }
 
@@ -36,20 +41,19 @@
 - (IBAction)dropButtonClick:(id)sender {
     [self.dropMenu show];
     
-    [self.dropMenu setAllColor:[UIColor blackColor]];
 }
 
 - (void)dropMenu:(YTDropMenu *)dropMenu didSelectIndexPath:(NSIndexSet *)selIndexPath{
     if([selIndexPath firstIndex] == 0){
-    
+        self.backgroundImgView.image = [UIImage imageNamed:@"a"];
     }
     
     if([selIndexPath firstIndex] == 1){
-    
+        self.backgroundImgView.image = [UIImage imageNamed:@"b"];
     }
     
-    if([selIndexPath firstIndex] == 1){
-    
+    if([selIndexPath firstIndex] == 2){
+        self.backgroundImgView.image = [UIImage imageNamed:@"c"];
     }
 }
 
